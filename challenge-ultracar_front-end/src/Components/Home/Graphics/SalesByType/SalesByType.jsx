5553bf59acc749e390a82ca4265a726763a2151d
Genesis-Henriques <GenesisHenriques@gmail.com>
2022-05-26 12:51:27 -0300

    :construction: The orders per month box was made using html + css

--- challenge-ultracar_front-end/src/Components/Home/Graphics/SalesByType/SalesByType.jsx
+++ challenge-ultracar_front-end/src/Components/Home/Graphics/SalesByType/SalesByType.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React from 'react';
+import Chart from 'react-google-charts';
+import './SalesByType.css';
+
+export const data = [
+  ["Vendedores", "Vendas", { role: 'annotation' }],
+  ["Vendas de peça", 1000, 1000],
+  ["Vendas de equipamento", 1170, 1170],
+  ["Manutenção", 660, 660],
+  ["Instalações", 1030, 1030],
+  ["Consultoria", 1030, 1030],
+];
+
+export const options = {
+  chartArea: {
+    backgroundColor: {
+        fill: '#FF0000',
+        fillOpacity: 0,
+     },
+  },
+  backgroundColor: {
+    fill: '#FF0000',
+    fillOpacity: 0
+  },
+  legend: { position: 'none' },
+  vAxis: {
+    textStyle:{color: '#FFF'},
+    gridlines: {
+      color: '#FFF',
+    },
+  },
+  hAxis: {
+    textStyle:{color: '#FFF'},
+    gridlines: {
+      color: 'none'
+    },
+  },
+  annotations: {
+    textStyle: {
+      // fontName: 'Open Sans',
+      color: 'white',
+    }
+  },
+  colors: ['#4B5FDA']
+};
 
 export default function SalesByType() {
   return (
-    <div>
-      <p>SalesByType</p>
+    <div className="sales-by-type">
+      <h3 id="sales-by-type-title">Vendas por Tipo de Serviço</h3>
+      <Chart
+        chartType="BarChart"
+        width="30em"
+        height="90%"
+        data={data}
+        options={options}
+      />
     </div>
   )
 }
